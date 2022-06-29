958c8694366ec34fac588aca528326cbeb8b62c8
Krzysztof Buzar <kbuzar@gmail.com>
2022-06-29 16:05:49 +0200

    note_seq added

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ from audio_to_midi import audio2midi
 import sounddevice as sd
 import soundfile as sf
 
-fs = 44100
-sd.default.samplerate = fs
-sd.default.channels = 1
-
-duration = 4  # seconds
-myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
-sf.write("new.wav", myrecording, fs)
-sd.wait()
+# fs = 44100
+# sd.default.samplerate = fs
+# sd.default.channels = 1
+#
+# duration = 4  # seconds
+# myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+# sf.write("new.wav", myrecording, fs)
+# sd.wait()
 
 
 s_time = time.time()
